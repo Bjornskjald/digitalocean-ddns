dbe7bad7a5a3550b6ac66f01912c66b3dfdaa3d7
Bjornskjald <github@bjorn.ml>
2018-12-08 23:08:21 +0100

    added more logging

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const DigitalOcean = require('do-wrapper').default
 const api = new DigitalOcean(API_KEY, 100)
 
 async function update () {
+  console.log('Updating...')
   const ip = await publicIp.v4()
-  console.log(ip)
+  console.log(`Your current IP address: ${ip}`)
+  console.log(`Searching for domain records for ${DOMAIN_NAME}...`)
   const { body: { domain_records } } = await api.domainRecordsGetAll(DOMAIN_NAME) // eslint-disable-line camelcase
+  console.log(`Found ${domain_records.length} records:`)
+  console.dir(domain_records)
   const record = domain_records.find(record => record.name === RECORD_NAME && record.type === 'A')
+  console.log(`Target record:`)
+  console.dir(record)
+  console.log(`Updating...`)
   await api.domainRecordsUpdate(DOMAIN_NAME, record.id, { data: ip })
-  console.log('Changed successfully!')
+  console.log('Updated successfully!')
 }
 update()
 setInterval(update, 10 * 60 * 1000)
